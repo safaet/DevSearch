46c20104f120f3e9d9ed214fb343f7a74b2f620e
safaet <safaetjaman@gmail.com>
2024-03-31 00:21:43 +0600

    001 URL_s

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def projects(request):
+    return HttpResponse('Here are our products')
+
+def project(request, pk):
+    return HttpResponse('SINGLE PROJECT' + ' ' + str(pk))
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('projects/', projects, name='projects'),
+    path('project/<str:pk>/', project, name="project")
 ]
