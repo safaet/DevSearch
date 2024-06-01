e8f832c9031860459ad0548272df3d030a0c2ff5
safaet <safaetjaman@gmail.com>
2024-06-01 16:08:15 +0600

    002 Add_Render Profile

--- users/models.py
+++ users/models.py
@@ -9,9 +9,10 @@ class Profile(models.Model):
     name = models.CharField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length=500, blank=True, null=True)
     username = models.CharField(max_length=200, blank=True, null=True)
+    location = models.CharField(max_length=200, blank=True, null=True)
     short_intro = models.CharField(max_length=500, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
-    Profile_image = models.ImageField(null=True, blank=True, upload_to='profiles/', default='profiles/user-default.png')
+    profile_image = models.ImageField(null=True, blank=True, upload_to='profiles/', default='profiles/user-default.png')
     social_github = models.CharField(max_length=500, blank=True, null=True)
     social_twitter = models.CharField(max_length=500, blank=True, null=True)
     social_linkedin = models.CharField(max_length=500, blank=True, null=True)
@@ -23,3 +24,14 @@ class Profile(models.Model):
     
     def __str__(self):
         return str(self.user.username)
+    
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, 
+                          primary_key=True, editable=False)
+    
+    def __str__(self):
+        return str(self.name)
--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.profiles, name='profiles'),
+    path('user/<str:pk>', views.userProfile, name='user-profile'),
 ]
