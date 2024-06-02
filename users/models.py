491756e5bc726ba2e1a4eb987c34f419de4c2561
safaet <safaetjaman@gmail.com>
2024-06-02 11:47:53 +0600

    003 Signals

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ import uuid
 
 # Create your models here.
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, blank=True)
     name = models.CharField(max_length=200, blank=True, null=True)
@@ -23,7 +24,7 @@ class Profile(models.Model):
                           primary_key=True)
     
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
     
 class Skill(models.Model):
     owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.CASCADE)
@@ -35,3 +36,5 @@ class Skill(models.Model):
     
     def __str__(self):
         return str(self.name)
+
+
